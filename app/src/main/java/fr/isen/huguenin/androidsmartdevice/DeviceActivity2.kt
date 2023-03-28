443f75a9aa524a54395c75499bdad67eb4a08235
unknown <MathildeH-28>
2023-03-28 13:16:05 +0200

    led

--- app/src/main/java/fr/isen/huguenin/androidsmartdevice/DeviceActivity2.kt
+++ app/src/main/java/fr/isen/huguenin/androidsmartdevice/DeviceActivity2.kt
@@ -15,5 +15,38 @@ class DeviceActivity2 : AppCompatActivity() {
 
         binding = ActivityDevice2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        clickOnLed()
+    }
+
+    private fun clickOnLed() {
+        binding.led1.setOnClickListener {
+            if( binding.led1.imageTintList == getColorStateList(R.color.purple_200)) {
+                binding.led1.imageTintList = getColorStateList(R.color.black)
+            }
+            else {
+                binding.led1.imageTintList = getColorStateList(R.color.purple_200)
+            }
+        }
+
+        binding.led2.setOnClickListener {
+            if( binding.led2.imageTintList == getColorStateList(R.color.purple_200)) {
+                binding.led2.imageTintList = getColorStateList(R.color.black)
+            }
+            else {
+                binding.led2.imageTintList = getColorStateList(R.color.purple_200)
+            }
+        }
+
+
+        binding.led3.setOnClickListener {
+            if( binding.led3.imageTintList == getColorStateList(R.color.purple_200)) {
+                binding.led3.imageTintList = getColorStateList(R.color.black)
+            }
+            else {
+                binding.led3.imageTintList = getColorStateList(R.color.purple_200)
+            }
+        }
+
     }
 }
